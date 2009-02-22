6bb4fc04fb8489fa9d310304ecedf29c4693eff8
Russ Garrett <russ@garrett.co.uk>
2009-02-22 20:01:17 +0000

    Initial blurb

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hackspace Foundation</title>
+    <link rel="stylesheet" href="/css/main.css" />
+</head>
+<body>
+<h1>Hackspace Foundation</h1>
---
+++ index.php
@@ -0,0 +1,18 @@
+<? require_once('header.php'); ?>
+
+<p><strong>The Hackspace Foundation is a non-profit, community organisation dedicated to providing physical
+spaces for hacking in the UK.</strong></p>
+
+<p>We mean <em>hacking</em> in the <a href="http://catb.org/jargon/html/H/hacker.html">original sense</a> of the word 
+&mdash; the art of making, tinkering, improving and destroying things. Nothing to do with breaking into systems 
+(that's <em>cracking</em>), and not necessarily restricted to computers or electronics.</p>
+
+<p>Currently we've got two spaces in the early stages of formation:</p>
+<ul>
+    <li><a href="http://groups.google.com/group/london-hack-space">London Hack Space</a></li>
+    <li><a href="http://groups.google.com/group/birmingham-hack-space">Birmingham Hack Space</a></li>
+</ul>
+
+<p>If you're thinking of starting a hacker space in the UK, please get in touch with us - we'd love to help you.</p>
+
+<? require_once('footer.php'); ?>
