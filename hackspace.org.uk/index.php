336bb9fae5da71298d7d580f84be729412a9be5b
Russ Garrett <russ@garrett.co.uk>
2009-03-12 22:25:07 +0000

    Link to new starting page

--- hackspace.org.uk/index.php
+++ hackspace.org.uk/index.php
@@ -15,6 +15,6 @@ physical spaces for hacking in the UK.</strong></p>
     <li><a href="http://groups.google.com/group/birmingham-hack-space">Birmingham Hack Space</a></li>
 </ul>
 
-<p>If you're thinking of starting a hacker space in the UK, please <a href="/contact">get in touch</a> with us &mdash; we&apos;d love to help you.</p>
+<p>If you're thinking of starting a hacker space in the UK, <a href="/starting.php">we can help you</a>.</p>
 
 <? require('footer.php'); ?>
