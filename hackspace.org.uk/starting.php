ccfae3902a6028ecf1021ddeb43e8c5c6f7d68b8
Russ Garrett <russ@garrett.co.uk>
2009-03-12 22:18:19 +0000

    More info for prospective hackspaces.

---
+++ hackspace.org.uk/starting.php
@@ -0,0 +1,32 @@
+<? 
+$page = 'starting';
+require('header.php'); 
+?>
+<h2>Starting a Hackspace</h2>
+<p>
+    If you're thinking of starting a hackspace in the UK, we'd love to 
+    give you a hand. Here's what we can offer you:
+</p>
+
+<ul>
+    <li>Limited liability (this is pretty essential for renting a space)</li>
+    <li>A membership structure, and a system for managing it online</li>
+    <li>Methods for collecting subscriptions (we currently support standing orders and PayPal)</li>
+    <li>The possibility of a loan to get your space started</li>
+    <li>A helping hand from people who already run hackspaces</li>
+</ul>
+
+<p>
+    When joining the Hackspace Foundation as a member, members of your space will be 
+    given the option of "earmarking" the funds for your hackspace. Hackspaces get 
+    100% of the money earmarked for them (although we do have to take the tax 
+    payable on any profits you make).
+</p>
+
+<p>
+    If you're interested in taking us up on this offer, please get in touch with Russ at 
+    russ (at) hackspace (dot) org (dot) uk.
+</p>
+
+
+<? require('footer.php'); ?>
