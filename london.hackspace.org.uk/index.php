23ed4f3811a1546ce4d3641f881e93156b1d07bd
Russ Garrett <russ@garrett.co.uk>
2009-04-03 18:58:29 +0100

    Make front page better

--- london.hackspace.org.uk/index.php
+++ london.hackspace.org.uk/index.php
@@ -6,12 +6,24 @@ require('header.php'); ?>
 <p class="blurb"><strong>The London Hackspace project is working to create a permanent space in London for people to 
 come and make things.</strong></p>
 
-<p>We're currently building up funds to be able to rent a space. If you're interested, please <a href="/donate">donate</a>!</p>
-
 <p>We hold regular <a href="/events">hack evenings</a>, generally in a pub, to help build the community so people can get to know each other, 
 as well as to provide a place for geeks to come and hack.</p>
 
-<p>The London Hackspace is run by the <a href="http://hackspace.org.uk">Hackspace Foundation</a> &mdash; 
-a non-profit, community-run organisation dedicated to promoting hacker spaces in the UK.</p>
+<h2>We need money to make this happen</h2>
+
+<p>Renting a place in London isn't cheap &mdash; if you're interested, <strong>please help us out</strong> by
+donating using <a href="/donate">nank transfer</a> or Paypal:</p>
+<p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="padding-bottom:10px">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="4464387">
+<input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+</p>
+
+<h2>Join us!</h2>
+<p>If you're interested in helping set the hack space up, please consider becoming a 
+	<a href="http://hackspace.org.uk/membership.php">founding member</a> of the Hackspace Foundation.</p>
 
 <? require('footer.php'); ?>
