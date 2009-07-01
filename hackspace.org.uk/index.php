973edfc2b6e8da780c7c19e609d89c7bf024b43d
Russ Garrett <russ@garrett.co.uk>
2009-07-01 19:56:31 +0100

    add York

--- hackspace.org.uk/index.php
+++ hackspace.org.uk/index.php
@@ -23,6 +23,7 @@ to the lack of hacker spaces in the UK. Since then, we've helped groups in more 
     <li><a href="http://leeds.hackspace.org.uk">Leeds Hack Space</a></li>
     <li><a href="http://lists.psand.net/cgi-bin/mailman/listinfo/hackspacebristol">Bristol Hack Space</a></li>
     <li><a href="http://www.southackton.org.uk/">SoutHACKton (Southampton)</a></li>
+    <li><a href="http://groups.google.com/group/york-hack-space">York Hackspace</a></li>
 </ul>
 
 <p>If you're thinking of starting a hacker space in the UK, <a href="/starting.php">we can help you</a>.</p>
