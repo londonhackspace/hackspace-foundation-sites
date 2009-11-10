12cb4600f4459cb083e15a833a4a96af2c8e9d57
Russ <russ@garrett.co.uk>
2009-11-10 20:55:42 +0000

    Improved london front page

--- london.hackspace.org.uk/index.php
+++ london.hackspace.org.uk/index.php
@@ -3,12 +3,14 @@ $page = 'about';
 $desc = 'The London Hackspace project runs a dedicated hacker space in London, UK for people to learn and build things.';
 require('header.php'); ?>
 
-<p class="blurb"><strong>We run a dedicated space for people to learn and build things in London.</strong></p>
+<p class="blurb"><strong>We're a non-profit, collaboratively-run <a href="http://en.wikipedia.org/wiki/Hackerspace">hacker space</a> in central London. We provide infrastructure for our members to make and learn things.</strong></p>
 
 <p>We have a space near King's Cross which is open to our members, and we hold regular 
-<a href="/events">hack evenings and workshops</a> which are open to everyone.</p>
+free <a href="/events">hack evenings and workshops</a> which are open to everyone.</p>
 
-<p>To keep up to date with us, join the <a href="/irc">IRC channel</a>, 
+<p>Learn more about the London Hackspace on <a href="http://wiki.hackspace.org.uk">our wiki</a>.</p>
+
+<p>To keep up to date with us, join the <a href="http://webchat.freenode.net/?channels=london-hack-space">IRC channel</a>, 
     <a href="http://groups.google.com/group/london-hack-space">mailing list</a>, 
 or <a href="http://twitter.com/londonhackspace">follow us on twitter</a></p>
 
