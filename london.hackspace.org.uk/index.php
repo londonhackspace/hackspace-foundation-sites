5805d102e5dd863ba43ca07462a69391336cf622
Russ Garrett <russ@garrett.co.uk>
2009-11-15 02:32:32 +0000

    Frontpage revamp, take one

--- london.hackspace.org.uk/index.php
+++ london.hackspace.org.uk/index.php
@@ -1,6 +1,6 @@
 <? 
 $page = 'about';
-$desc = 'The London Hackspace project runs a dedicated hacker space in London, UK for people to learn and build things.';
+$desc = 'The London Hackspace runs a non-profit, collaboratively-run hacker space in central London. We provide infrastructure for our members to make and learn things.';
 require('header.php'); ?>
 
 <p class="blurb"><strong>We're a non-profit, collaboratively-run <a href="http://en.wikipedia.org/wiki/Hackerspace">hacker space</a> in central London. We provide infrastructure for our members to make and learn things.</strong></p>
@@ -8,22 +8,17 @@ require('header.php'); ?>
 <p>We have a space near King's Cross which is open to our members, and we hold regular 
 free <a href="/events">hack evenings and workshops</a> which are open to everyone.</p>
 
-<p>Learn more about the London Hackspace on <a href="http://wiki.hackspace.org.uk">our wiki</a>.</p>
+<p><a href="http://wiki.hackspace.org.uk"><img src="/images/electronics.jpg" /></a>
+<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=7&display=random&size=s&layout=h&context=in%2Fpool-1183951%40N23%2F&source=group&group=1183951%40N23"></script></p>
+<p><strong>Learn more about the London Hackspace on <a href="http://wiki.hackspace.org.uk">our wiki</a>.</strong></p>
 
-<p>To keep up to date with us, join the <a href="http://webchat.freenode.net/?channels=london-hack-space">IRC channel</a>, 
+<p>To get involved, join the <a href="http://webchat.freenode.net/?channels=london-hack-space">IRC channel</a>, 
     <a href="http://groups.google.com/group/london-hack-space">mailing list</a>, 
-or <a href="http://twitter.com/londonhackspace">follow us on twitter</a></p>
+or <a href="http://twitter.com/londonhackspace">follow us on twitter</a>.</p>
 
 <br>
 <h2>Join us!</h2>
 <p>If you want to use our space you must <a href="/donate">subscribe</a>, however attending 
-our <a href="/events">bi-weekly hack-evenings</a> is free!</p>
-
-<br>
-
-<h2>Media</h2>
-<p><script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=6&display=latest&size=s&layout=h&context=in%2Fpool-1183951%40N23%2F&source=group&group=1183951%40N23"></script></p>
-<p>Take a look at <a href="http://www.flickr.com/groups/1183951@N23/pool/">our flickr group</a> to 
-see <a href="http://www.flickr.com/groups/1183951@N23/pool/">more photos from hackspace events</a>.</p>
+our <a href="/events">events</a> is free!</p>
 
 <? require('footer.php'); ?>
