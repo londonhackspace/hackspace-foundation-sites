328a967fb60516eb47faf9589c512c518939b438
Russ Garrett <russ@garrett.co.uk>
2009-12-06 21:38:00 +0000

    November Report

---
+++ london.hackspace.org.uk/reports/200911.php
@@ -0,0 +1,49 @@
+<? 
+$page = 'reports';
+$title = 'November 2009';
+require('../header.php'); 
+?>
+<h2>Hackspace Foundation report &mdash; November 2009</h2>
+
+<p>Rent is up to £420/month in consideration of the electricity used for heating.</p>
+
+<h3>Stats</h3>
+<table>
+    <tr><th>Registered Users</th><td>97</td><td>(0)</td></tr>
+    <tr><th>Foundation Members</th><td>38</td><td>(-1)</td></tr>
+</table>
+
+<h3>Income Statement</h3>
+<table class="financial">
+    <tr><th colspan="2">Revenues</th></tr>
+    <tr><td class="sub">Donations</td><td class="amount">£191.15</td></tr>
+    <tr><td class="sub">Interest</td><td class="amount">£0.22</td></tr>
+    <tr><td class="sub">Subscriptions</td><td class="amount">£815.00</td></tr>
+    <tr><td>Total Revenue</td><td class="amount">£1,006.37</td></tr>
+    <tr><th colspan="2">Expenses</th></tr>
+    <tr><td class="sub">Entertainment</td><td class="amount">£182.20</td></tr>
+    <tr><td class="sub">Equipment</td><td class="amount">£122.54</td></tr>
+    <tr><td class="sub">Rent</td><td class="amount">£420.00</td></tr>
+    <tr><td>Total Expenses</td><td class="amount">£724.74</td></tr>
+    <tr><th>Net Income</th><td class="amount">£281.63</td></tr>
+    <tr></tr>
+</table>
+
+<h3>Balance Sheet</h3>
+<table class="financial">
+    <tr><th colspan="2">Assets</th></tr>
+    <tr><th colspan="2" class="sub">Current Assets</th></tr>
+    <tr><td class="sub2">Cash in Bank</td><td class="amount">£3,584.20</td></tr>
+    <tr><td class="sub">Fixed Assets</td><td class="amount">£671.90</td></tr>
+    <tr><td>Total Assets</td><td class="amount">£4,256.10</td></tr>
+    <tr><th colspan="2">Liabilities</th></tr>
+    <tr><td class="sub2">Donations (Leeds)</td><td class="amount">£726.90</td></tr>
+    <tr><td>Total Liabilities</td><td class="amount">£726.90</td></tr>
+    <tr><th colspan="2">Equity</th></tr>
+    <tr><td>Retained Earnings</td><td class="amount">£3,529.20</td></tr>
+    <tr><td>Total Equity</td><td class="amount">£3,529.20</td></tr>
+    <tr><th>Total Liabilites and Equity</th><td class="amount">£4,256.10</td></tr>
+    <tr></tr>
+</table>
+
+<? require('../footer.php'); ?>
