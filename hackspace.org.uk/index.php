9c103b4105ad91dd0e2188793e17d6dabf6b535c
Russ Garrett <russ@garrett.co.uk>
2009-12-25 13:43:10 +0000

    Change Bristol link

--- hackspace.org.uk/index.php
+++ hackspace.org.uk/index.php
@@ -21,7 +21,7 @@ many other spaces in the UK. Currently we know of the following spaces:</p>
     <li><a href="http://www.fizzpop.org.uk">fizzPOP (Birmingham)</a></li>
     <li><a href="http://groups.google.com/group/NW-hack-space">North West Hackspace</a></li>
     <li><a href="http://leeds.hackspace.org.uk">Leeds Hack Space</a></li>
-    <li><a href="http://lists.psand.net/cgi-bin/mailman/listinfo/hackspacebristol">Bristol Hack Space</a></li>
+    <li><a href="http://bristol.hackspace.org.uk">Bristol Hack Space</a></li>
     <li><a href="http://www.southackton.org.uk/">SoutHACKton (Southampton)</a></li>
     <li><a href="http://groups.google.com/group/york-hack-space">York Hackspace</a></li>
     <li><a href="http://stockport.hackspace.org.uk">Stockport/South-east Manchester Hackspace</a></li>
