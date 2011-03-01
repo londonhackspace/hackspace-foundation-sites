a1e0272568ecb62ee29f8b0a5acb7a55aa68898b
root <root@hackspace.vps.bitfolk.com>
2011-03-01 15:56:08 +0000

    Add iCal feed link

--- london.hackspace.org.uk/events/index.php
+++ london.hackspace.org.uk/events/index.php
@@ -8,6 +8,6 @@ require('../header.php'); ?>
 <iframe src="http://www.google.com/calendar/embed?showCalendars=0&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=gc1bopmh3c5n0ogvlo6ceujlkc%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=Europe%2FLondon" style=" border-width:0 " width="576" height="400" frameborder="0" scrolling="no"></iframe>
 
 <br><br>
-<p>You can view past events in our <a href="http://upcoming.yahoo.com/group/15565/history/">upcoming group</a>.</p>
+<p>You can view past events in our <a href="http://upcoming.yahoo.com/group/15565/history/">upcoming group</a>, or add <a href="http://www.google.com/calendar/ical/gc1bopmh3c5n0ogvlo6ceujlkc%40group.calendar.google.com/public/basic.ics">add the iCal feed to your calendar</a>.</p>
 
 <? require('../footer.php'); ?>
