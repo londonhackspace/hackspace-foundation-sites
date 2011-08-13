0fa4b3e63cdc8997871568439e4a76b22dd3e198
Russ Garrett <russ@garrett.co.uk>
2011-08-13 14:00:52 +0100

    Restore cam control

--- london.hackspace.org.uk/members/webcams.php
+++ london.hackspace.org.uk/members/webcams.php
@@ -31,6 +31,19 @@ if (!isset($_GET['camera'])) {
 <div style="padding-top:10px">
 <? if ($live) { ?>
     <img src="camera.php?id=<?=$camera?>">
+    <? if ($camera == 3 or $camera == 2) {
+        if ($camera == 3) { $file = 'camcontrol2.php'; } else { $file = 'camcontrol.php'; }
+        ?>
+        <small>
+        <a href="http://hack.rs/<?=$file?>?cmd=left" target="camcontrol">Left</a> |
+        <a href="http://hack.rs/<?=$file?>?cmd=right" target="camcontrol">Right</a> |
+        <a href="http://hack.rs/<?=$file?>?cmd=up" target="camcontrol">Up</a> |
+        <a href="http://hack.rs/<?=$file?>?cmd=down" target="camcontrol">Down</a><br>
+        <a href="http://hack.rs/<?=$file?>?cmd=panscan" target="camcontrol">Pan Scan</a> |
+        <a href="http://hack.rs/<?=$file?>?cmd=tiltscan" target="camcontrol">Tilt Scan</a>
+        </small>
+        <iframe name='camcontrol' frameBorder="0" width="0" height="0"></iframe>
+    <? } ?>
 <? } else { ?>
     <img src="/cam_snapshots/<?=$camera?>.jpg">
 <? } ?>
