245e35c59733c12dad5c7e790dba41d147484c6d
Russ Garrett <russ@garrett.co.uk>
2011-08-13 14:03:34 +0100

    Fix camera numbering

--- london.hackspace.org.uk/members/webcams.php
+++ london.hackspace.org.uk/members/webcams.php
@@ -31,8 +31,8 @@ if (!isset($_GET['camera'])) {
 <div style="padding-top:10px">
 <? if ($live) { ?>
     <img src="camera.php?id=<?=$camera?>">
-    <? if ($camera == 3 or $camera == 2) {
-        if ($camera == 3) { $file = 'camcontrol2.php'; } else { $file = 'camcontrol.php'; }
+    <? if ($camera == 4 or $camera == 2) {
+        if ($camera == 4) { $file = 'camcontrol2.php'; } else { $file = 'camcontrol.php'; }
         ?>
         <small>
         <a href="http://hack.rs/<?=$file?>?cmd=left" target="camcontrol">Left</a> |
