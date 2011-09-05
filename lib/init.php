d09b4e48b76a10ef6e9790e0e4eccd12330d4622
Russ Garrett <russ@garrett.co.uk>
2011-09-06 00:26:00 +0100

    Use our own session directory

--- lib/init.php
+++ lib/init.php
@@ -9,6 +9,7 @@ $db = new fDatabase('sqlite', dirname(__FILE__) . '/../var/database.db');
 fORMDatabase::attach($db);
 
 fSession::setLength('1 day');
+fSession::setPath(dirname(__FILE__) . '/../var/session');
 
 if ($uid = fSession::get('user')) {
     $user = new User($uid);
