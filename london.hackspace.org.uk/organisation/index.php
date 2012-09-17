c3f3fd3c67b90c4f7a5153a4e3923dfe5e7a48d9
martind <martin@dekstop.de>
2012-09-17 13:43:21 +0100

    Adding community contact details

--- london.hackspace.org.uk/organisation/index.php
+++ london.hackspace.org.uk/organisation/index.php
@@ -21,6 +21,12 @@ require('../header.php');
 
 <p>Members of the company are entitled to vote at meetings, and can elect directors.</p>
 
+<h3>Getting in Touch</h3>
+
+<p>There are several ways of getting in contact with London Hackspace members and start a first conversation, be it that you want to learn more about our community, you are interested in coming to our space, you want to discuss your project with someone, or for any other reason.</p>
+
+<p>We have a number of very active <a href="http://wiki.london.hackspace.org.uk/view/Mailing_List">mailing lists</a> for the community and its subgroups, an <a href="http://wiki.london.hackspace.org.uk/view/IRC">IRC channel</a> on Freenode, but also a weekly <a href="http://wiki.london.hackspace.org.uk/view/Weekly_Open_Evenings">social night on every Tuesday</a> that is open to the public.</p>
+
 <h3>Directors</h3>
 
 <p>The directors of the company carry out the day-to-day business and are elected by the members yearly.
