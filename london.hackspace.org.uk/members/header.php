51b93c439d372dd6de7b4f59bebe664669f94f64
Oskar Pearson <oskar@deckle.co.uk>
2013-04-16 21:17:16 +0100

    Removed unnecessary file, as it's no longer used

--- london.hackspace.org.uk/members/header.php
+++
@@ -1,7 +0,0 @@
-<? 
-require('../header.php');
-?>
-<ul id="submenu">
-    <?=menulink('/organisation/arts.php', 'arts', 'Articles of Association');?>
-    <?=menulink('/organisation/mem.php', 'mem', 'Memorandum of Association');?>
-</ul>
