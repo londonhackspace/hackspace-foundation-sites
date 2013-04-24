7081aa53bc25c14eb3a990462213ffa3f35a223d
Russ Garrett <russ@garrett.co.uk>
2013-04-24 22:32:27 +0100

    Remove hackspace.org.uk directory, it's no longer used

--- hackspace.org.uk/irc/index.php
+++
@@ -1,9 +0,0 @@
-<? 
-$page = 'irc';
-$title = 'IRC';
-require('../header.php'); ?>
-<h2>IRC</h2>
-<p>You can come and chat to members of all the spaces in <a href="http://webchat.freenode.net/?channels=hackspace">#hackspace</a> on <b>irc.freenode.net</b>.</p>
-<p>If you want to try and start a new space, you can talk to us in <a href="http://webchat.freenode.net/?channels=hackspacefoundation">#hackspacefoundation</a> on <b>irc.freenode.net</b>.</p>
-
-<? require('../footer.php'); ?>
