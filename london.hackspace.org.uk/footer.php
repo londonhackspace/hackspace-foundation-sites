db0351a89377d69fc7383eba4ebb441276ca8bdf
Russ Garrett <russ@garrett.co.uk>
2013-06-05 09:54:34 +0100

    Update to async google anayltics footer

--- london.hackspace.org.uk/footer.php
+++ london.hackspace.org.uk/footer.php
@@ -14,20 +14,6 @@
     </div>
 </div><!-- doc -->
 
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-
-<script type="text/javascript">
-try {
-    var pageTracker = _gat._getTracker("UA-7698227-2");
-    <? if ($user) {?>
-        pageTracker._setVar("Logged In");
-    <? }?>
-    pageTracker._trackPageview();
-} catch(err) {}</script>
-
 <? if (isset($show_twitter_feed)) { ?>
 <script>
 !function(d,s,id){
@@ -41,6 +27,21 @@ try {
 }(document,"script","twitter-wjs");
 </script>
 <? } ?>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7698227-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 </body>
 </html>
