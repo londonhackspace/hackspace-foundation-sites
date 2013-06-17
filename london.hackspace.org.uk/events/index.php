b675860e919eb69c250bfff57cd12b839efb4072
Mark Steward <marksteward@gmail.com>
2013-06-17 18:15:39 +0100

    Fix calendar insecure content

--- london.hackspace.org.uk/events/index.php
+++ london.hackspace.org.uk/events/index.php
@@ -5,7 +5,7 @@ require('../header.php'); ?>
 
 <h2>Events</h2>
 
-<iframe src="http://www.google.com/calendar/embed?showCalendars=0&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=gc1bopmh3c5n0ogvlo6ceujlkc%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=Europe%2FLondon" style=" border-width:0 " width="576" height="400" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://www.google.com/calendar/embed?showCalendars=0&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=gc1bopmh3c5n0ogvlo6ceujlkc%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=Europe%2FLondon" style=" border-width:0 " width="576" height="400" frameborder="0" scrolling="no"></iframe>
 
 <br /><br />
 
