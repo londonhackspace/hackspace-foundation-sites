d63157d8df98fc55dbc4d715615c89ed7e4a5d72
Russ Garrett <russ@garrett.co.uk>
2013-11-16 14:31:17 +0000

    Re-jig donations page

--- london.hackspace.org.uk/donate.php
+++ london.hackspace.org.uk/donate.php
@@ -4,7 +4,26 @@ require( './header.php' );
 ?>
 
 <h2>Donate</h2>
-<p>Here's how to donate to London Hackspace.</p>
+<p>London Hackspace is a non-profit organisation which provides facilites and hosts events for the geek community in London.
+    It costs us <a href="/cost-of-hacking/">more than £10,000 per month</a>
+    to keep our doors open. Your donation can help us make our space more comfortable, buy better tools,
+    and run more events.</p>
+
+<p>Here's how to donate to London Hackspace:</p>
+
+<h3>Paypal</h3>
+<p>
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="3741369" />
+        <input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donateCC_LG.gif" style="border:0;" name="submit" alt="PayPal - The safer, easier way to pay online." />
+        <img alt="" style="border:0;" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
+    </form>
+</p>
+
+
+<h3>Bitcoin</h3>
+<p><a class="coinbase-button" data-code="02f847c530bf2538491bb3c3da5dd851" data-button-style="custom_large" href="https://coinbase.com/checkouts/02f847c530bf2538491bb3c3da5dd851">Donate Bitcoins</a><script src="https://coinbase.com/assets/button.js" type="text/javascript"></script></p>
 
 <h3>Bank Transfer</h3>
 <? if ($user) { ?>
@@ -34,20 +53,5 @@ require( './header.php' );
 <? } else { ?>
 <p>Due to direct debit fraud, we require you to <a href="/login.php">log in</a> before you can see our bank details.</p>
 <? } ?>
-
-
-<h3>Paypal</h3>
-<div>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-        <input type="hidden" name="cmd" value="_s-xclick" />
-        <input type="hidden" name="hosted_button_id" value="3741369" />
-        <input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donateCC_LG.gif" style="border:0;" name="submit" alt="PayPal - The safer, easier way to pay online." />
-        <img alt="" style="border:0;" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
-    </form>
-</div>
-
-
-<h3>Bitcoin</h3>
-<p><a class="coinbase-button" data-code="02f847c530bf2538491bb3c3da5dd851" data-button-style="custom_large" href="https://coinbase.com/checkouts/02f847c530bf2538491bb3c3da5dd851">Donate Bitcoins</a><script src="https://coinbase.com/assets/button.js" type="text/javascript"></script></p>
 
 <?php require('./footer.php'); ?>
