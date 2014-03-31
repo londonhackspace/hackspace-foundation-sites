6e802398aaa7a70f5427c76f6d313da5ffd8077b
Russ Garrett <russ@garrett.co.uk>
2014-03-31 20:45:11 +0100

    Proper CSS for webcam thumbnails

--- london.hackspace.org.uk/members/webcams.php
+++ london.hackspace.org.uk/members/webcams.php
@@ -20,19 +20,18 @@ if (!isset($user) || !$user->isMember()) {
     fURL::redirect('/login.php?forward=/members/webcams.php');
 }
 ?>
-<div style="float: left; padding-left:20px; padding-right:20px;">
+<div id="webcam-grid">
 <?
 foreach ($cameras as $id => $name) {
 
 ?>
-<div style="float:left; height:320px; width:362px; margin: 1px; border: 1px #ccc solid;">
+<div class="webcam-image thumbnail with-caption">
   <a href="camera.php?id=<?=$id?>">
-    <img src="camera.php?id=<?=$id?>&scale=50" style="display:block; margin: 0 auto;" alt="<?=$name?>">
-    <p style="text-align: center"><?=$name?></p>
+    <img src="camera.php?id=<?=$id?>&scale=50" alt="<?=$name?>">
+    <p><?=$name?></p>
   </a>
 </div>
 
 <? } ?>
 </div>
-<div style="clear:both"></div>
 <? require('../footer.php'); ?>
