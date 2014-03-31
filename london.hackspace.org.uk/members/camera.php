5ffe13f73fcd4012b19191ae82df1dcd24e1d544
Russ Garrett <russ@garrett.co.uk>
2014-03-31 19:03:19 +0100

    New webcams page

--- london.hackspace.org.uk/members/camera.php
+++ london.hackspace.org.uk/members/camera.php
@@ -1,6 +1,9 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php');
 
+$host = 'http://[2001:8b0:856:1::31]';
+$scale = 80;
+
 if (!$user || !$user->isMember()) {
     header('HTTP/1.1 403 Forbidden');
     die();
@@ -10,9 +13,17 @@ if (!isset($_GET['id'])) {
     header('HTTP/1.1 400 Bad Request');
 }
 
-apache_setenv('PHP_AUTH', '1');
+if (!isset($_GET['scale'])) {
+    $scale = 100;
+} else {
+    $scale = intval($_GET['scale']);
+}
 
-header('Content-Type: multipart/x-mixed-replace; boundary=--BoundaryString');
-virtual('/cameras/' . intval($_GET['id']));
+
+header('Content-Type: image/jpeg');
+
+$chan = intval($_GET['id']);
+
+echo(file_get_contents("$host/cgi-bin/nph-zms?mode=single&scale=$scale&monitor=$chan"));
 
 ?>
--- london.hackspace.org.uk/members/webcams.php
+++ london.hackspace.org.uk/members/webcams.php
@@ -1,54 +1,38 @@
-<? 
+<?
 $page = 'webcams';
 $title = "Webcams";
 $noindex = True;
 $desc = '';
 require('../header.php');
 
-$live = False;
+$cameras = array(
+  19 => "Main Room",
+  18 => "Classroom",
+  17 => "Workshop",
+  14 => "Front Door",
+  13 => "Back Lobby",
+  12 => "Back Door",
+  10 => "Back Yard",
+  9 => "Back Gate"
+);
 
-if (isset($user) && $user->isMember() && !isset($_GET['nostream'])) {
-    $live = True;
+if (!isset($user) || !$user->isMember()) {
+    fURL::redirect('/login.php?forward=/members/webcams.php');
 }
-
-if (!isset($_GET['camera'])) {
-    $camera = 1;
-} else {
-    $camera = intval($_GET['camera']);
-}
+?>
+<div style="float: left; padding-left:20px; padding-right:20px;">
+<?
+foreach ($cameras as $id => $name) {
 
 ?>
-<? if (!$live && !isset($_GET['nostream'])) { ?>
-    <p><strong>You're not a member, so you're seeing a delayed snapshot of our webcams.</strong></p>
-<? } ?>
-<div>
-    <b>Switch Camera:</b>
-    <a href="/members/webcams.php?camera=1">Main</a> |
-    <a href="/members/webcams.php?camera=5">Classroom</a> |
-<!--    <a href="/members/webcams.php?camera=6">Quiet Room</a> | -->
-    <a href="/members/webcams.php?camera=4">Workshop</a> <!-- |
-    <a href="/members/webcams.php?camera=3">Door</a> -->
+<div style="float:left; height:320px; width:362px; margin: 1px; border: 1px #ccc solid;">
+  <a href="camera.php?id=<?=$id?>">
+    <img src="camera.php?id=<?=$id?>&scale=50" style="display:block; margin: 0 auto;" alt="<?=$name?>">
+    <p style="text-align: center"><?=$name?></p>
+  </a>
 </div>
 
-<div style="padding-top:10px">
-<? if ($live) { ?>
-    <img src="camera.php?id=<?=$camera?>" alt="Hackspace webcam feed" />
-    <? if ($camera == 4 or $camera == 2) {
-        if ($camera == 4) { $file = 'camcontrol2.php'; } else { $file = 'camcontrol.php'; }
-        ?>
-        <small>
-            <a href="http://hack.rs/<?=$file?>?cmd=left" target="camcontrol">Left</a> |
-            <a href="http://hack.rs/<?=$file?>?cmd=right" target="camcontrol">Right</a> |
-            <a href="http://hack.rs/<?=$file?>?cmd=up" target="camcontrol">Up</a> |
-            <a href="http://hack.rs/<?=$file?>?cmd=down" target="camcontrol">Down</a><br>
-            <a href="http://hack.rs/<?=$file?>?cmd=panscan" target="camcontrol">Pan Scan</a> |
-            <a href="http://hack.rs/<?=$file?>?cmd=tiltscan" target="camcontrol">Tilt Scan</a>
-        </small>
-
-        <iframe name='camcontrol' frameBorder="0" width="0" height="0"></iframe>
-    <? } ?>
-<? } else { ?>
-    <img src="/cam_snapshots/<?=$camera?>.jpg" alt="Hackspace webcam feed" />
 <? } ?>
 </div>
+<div style="clear:both"></div>
 <? require('../footer.php'); ?>
