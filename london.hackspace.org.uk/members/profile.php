d700c02f87731f4dd50a28c71094ae1d2baaa4ca
Sarah Simmonds <sarah@chixor.net>
2014-04-12 13:50:35 +0100

    Removed erroneous variable on IRC link

--- london.hackspace.org.uk/members/profile.php
+++ london.hackspace.org.uk/members/profile.php
@@ -76,7 +76,7 @@ if(
 					case 'Twitter': echo '<a target="_blank" href="https://twitter.com/' . $alias->getUsername() . '">' . $alias->getUsername() . '</a>'; break;
 					case 'LinkedIn': echo '<a target="_blank" href="http://www.linkedin.com/in/' . $alias->getUsername() . '">' . $alias->getUsername() . '</a>'; break;
 					case 'Flickr': echo '<a target="_blank" href="https://www.flickr.com/photos/' . $alias->getUsername() . '">' . $alias->getUsername() . '</a>'; break;
-					case 'IRC': echo '<a target="_blank" href="http://webchat.freenode.net/?channels=london-hack-space' . $alias->getUsername() . '">' . $alias->getUsername() . '</a>'; break;
+					case 'IRC': echo '<a target="_blank" href="http://webchat.freenode.net/?channels=london-hack-space">' . $alias->getUsername() . '</a>'; break;
 					case 'Hackspace Wiki': echo '<a target="_blank" href="https://wiki.london.hackspace.org.uk/view/User:' . $alias->getUsername() . '">' . $alias->getUsername() . '</a>'; break;
 					default: echo $alias->getUsername();
 				}?>
