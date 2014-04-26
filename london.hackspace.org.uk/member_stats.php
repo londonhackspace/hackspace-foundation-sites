69c837b27827bdf9e0ff224c56fbbd8278586f10
Mark Steward <marksteward@gmail.com>
2014-04-26 19:21:14 +0000

    Include last reconcile time in stats

--- london.hackspace.org.uk/member_stats.php
+++ london.hackspace.org.uk/member_stats.php
@@ -4,5 +4,6 @@
 
     $subscribers = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=1' )->fetchRow();
     $pending = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=0' )->fetchRow();
+    $last = $db->query('select strftime(\'%s\', max(timestamp)) AS num from transactions')->fetchRow();
  
-    print "subscribed:{$subscribers['num']} pending:{$pending['num']}";
+    print "subscribed:{$subscribers['num']} pending:{$pending['num']} last:{$last['num']}";
