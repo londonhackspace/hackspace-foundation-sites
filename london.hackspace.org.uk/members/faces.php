1b609bfe7692bea73eec33bd9100c1d66149fb40
Sarah Simmonds <sarah@chixor.net>
2014-05-01 00:10:01 +0100

    Cleaned up faces page

--- london.hackspace.org.uk/members/faces.php
+++ london.hackspace.org.uk/members/faces.php
@@ -4,11 +4,7 @@ require( '../header.php' );
 
 ensureLogin();
 
-$last = $db->query('SELECT max(date(timestamp)) FROM transactions')->fetchScalar();
-$include_unsubscribed = ($user->isAdmin() && isset($_GET['unsubscribed']) && $_GET['unsubscribed'] == 'on') ? true : false;
-?>
-
-<? if($user->isMember()) { 
+if($user->isMember()) { 
     $newUsersCount = 0;
     $newUsers = $db->translatedQuery( "SELECT DISTINCT t1.user_id, photo, full_name, (SELECT timestamp FROM transactions AS t2 WHERE t1.user_id = t2.user_id ORDER BY t2.timestamp ASC LIMIT 1) AS first FROM transactions AS t1, users, users_profiles WHERE t1.user_id = users.id AND users.id = users_profiles.user_id AND first like '".date('Y-m')."%' AND photo != '' ORDER BY t1.user_id DESC;");
     if($newUsers->countReturnedRows() > 0) {
@@ -21,17 +17,16 @@ $include_unsubscribed = ($user->isAdmin() && isset($_GET['unsubscribed']) && $_G
             	<a href="/members/profile/<?=$user['user_id']?>"><img style="max-width: 80px;" src="/members/photo.php?name=<?=$user['photo'] ?>&amp;size=med" alt="<?=$user['full_name'] ?>" title="<?=$user['full_name']?>"/></a>
         <?	}
         } 
-    } ?>
+    }
 
-<h2>The faces of London Hackspace.</h2> 
+    $users = $db->translatedQuery( "SELECT id, photo, full_name FROM users JOIN users_profiles ON (id=user_id) WHERE photo != '' ORDER BY lower(full_name)"); ?>
 
-        <?php
-        $users = $db->translatedQuery( "SELECT id, photo, full_name FROM users JOIN users_profiles ON (id=user_id) WHERE photo != '' ORDER BY lower(full_name)");
-        foreach( $users as $user ) { ?>
+<h2><?=$users->countReturnedRows()?> faces of London Hackspace.</h2> 
+
+<?  foreach( $users as $user ) { ?>
             	<a href="/members/profile/<?=$user['id']?>"><img style="max-width: 80px;padding-bottom:5px;" src="/members/photo.php?name=<?=$user['photo'] ?>&amp;size=med" alt="<?=$user['full_name'] ?>" title="<?=$user['full_name']?>"/></a>
-        <? } ?>
-
-<? } else { ?>
+<? } 
+ } else { ?>
    <p>You must be a member to use this page.</p>
 <?php } 
 
