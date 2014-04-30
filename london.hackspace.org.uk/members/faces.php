e0913557eeb2ee0be778e0f0622ef931c6476a32
Sarah Simmonds <sarah@chixor.net>
2014-05-01 00:31:06 +0100

    Removed photos of ppl who have disabled their profile

--- london.hackspace.org.uk/members/faces.php
+++ london.hackspace.org.uk/members/faces.php
@@ -6,7 +6,7 @@ ensureLogin();
 
 if($user->isMember()) { 
     $newUsersCount = 0;
-    $newUsers = $db->translatedQuery( "SELECT DISTINCT t1.user_id, photo, full_name, (SELECT timestamp FROM transactions AS t2 WHERE t1.user_id = t2.user_id ORDER BY t2.timestamp ASC LIMIT 1) AS first FROM transactions AS t1, users, users_profiles WHERE t1.user_id = users.id AND users.id = users_profiles.user_id AND first like '".date('Y-m')."%' AND photo != '' ORDER BY t1.user_id DESC;");
+    $newUsers = $db->translatedQuery( "SELECT DISTINCT t1.user_id, photo, full_name, (SELECT timestamp FROM transactions AS t2 WHERE t1.user_id = t2.user_id ORDER BY t2.timestamp ASC LIMIT 1) AS first FROM transactions AS t1, users, users_profiles WHERE t1.user_id = users.id AND users.id = users_profiles.user_id AND first like '".date('Y-m')."%' AND photo != '' AND users.disabled_profile = 0 ORDER BY t1.user_id DESC;");
     if($newUsers->countReturnedRows() > 0) {
 ?>
 <h4>New members in <?=date('F')?>, go up and say hi!</h4> 
@@ -19,7 +19,7 @@ if($user->isMember()) {
         } 
     }
 
-    $users = $db->translatedQuery( "SELECT id, photo, full_name FROM users JOIN users_profiles ON (id=user_id) WHERE photo != '' ORDER BY lower(full_name)"); ?>
+    $users = $db->translatedQuery( "SELECT id, photo, full_name FROM users JOIN users_profiles ON (id=user_id) WHERE photo != '' AND users.disabled_profile = 0 ORDER BY lower(full_name)"); ?>
 
 <h2><?=$users->countReturnedRows()?> faces of London Hackspace.</h2> 
 
