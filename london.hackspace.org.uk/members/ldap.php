c61240832499954b1bdfa07cf1d9e157aebf1fd6
Jasper Wallace <jasper@pointless.net>
2014-09-20 14:15:47 +0100

    describe the password fields a little better

--- london.hackspace.org.uk/members/ldap.php
+++ london.hackspace.org.uk/members/ldap.php
@@ -278,8 +278,8 @@ if($user->isMember()) {
     ?>
 
     <input id="username"      type=text     size=32 value="<? echo htmlspecialchars($user->getLdapuser()); ?>"><label for="username">LDAP Username</label><br />
-    <input id="ssha_password" type=password size=32><label for="ssha_password">SSHA Password</label><br />
-    <input id="nt_password"   type=password size=32><label for="nt_password">NT Password</label><br />
+    <input id="ssha_password" type=password size=32><label for="ssha_password">Password for general use (Will be converted to an SSHA hash).</label><br />
+    <input id="nt_password"   type=password size=32><label for="nt_password">Password for spacefed (Will be converted to an NTLMv2 hash).</label><br />
     <select id="shell">
     <? 
     foreach ($shells as $shell) {
