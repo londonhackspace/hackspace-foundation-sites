0fe40d014e00094d46a4b0e72bad2d4ecbef080e
Russ Garrett <russ@garrett.co.uk>
2014-11-09 00:26:26 +0000

    Include the right header

--- london.hackspace.org.uk/kiosk/index.php
+++ london.hackspace.org.uk/kiosk/index.php
@@ -1,6 +1,6 @@
 <?
 $page = 'kiosk';
-require('header.php');
+require('../header-minimal.php');
 ?>
 <h2>London Hackspace Membership Kiosk</h2>
 <p>This kiosk will soon allow you to add your RFID card, print Do Not Hack labels, and do other useful things. We're still working on it - if you're interested talk to Russ or Jasper.</p>
