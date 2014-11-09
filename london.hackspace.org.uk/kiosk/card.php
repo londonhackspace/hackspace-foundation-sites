3f817b46c859e9629be7acab66995dea743c22a5
Russ Garrett <russ@garrett.co.uk>
2014-11-09 16:13:07 +0000

    Rudimentary membership details

--- london.hackspace.org.uk/kiosk/card.php
+++ london.hackspace.org.uk/kiosk/card.php
@@ -3,13 +3,21 @@ require('./header.php');
 $cards = fRecordSet::build('Card', array('uid=' => $_GET['cardid']));
 if($cards->count() == 0) {
     //TODO: add card
+    print "nonexistent card";
     exit;
 }
-$card = $cards[0];
+$card = $cards->getRecord(0);
+$user = new User($card->getUserId());
+$user->load();
 
 ?>
 <h2>Membership Management</h2>
-Card ID: <?=$_GET['cardid']?>
+<p>
+Member Name: <?=$user->prepareFullName()?><br>
+Member ID: <?=$user->getMemberNumber()?><br>
+Card ID: <?=$card->prepareUid()?><br>
+Subscribed: <?=$user->isMember()?"Yes":"No"?>
+</p>
 
 
 <a href="/kiosk/" class="btn btn-default">Go back</a>
