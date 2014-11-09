b641b04564b6bdf81305893c5c13aa022f01950f
Russ Garrett <russ@garrett.co.uk>
2014-11-09 15:38:01 +0000

    More kiosk code

--- london.hackspace.org.uk/kiosk/card.php
+++ london.hackspace.org.uk/kiosk/card.php
@@ -1,5 +1,16 @@
 <?
 require('./header.php');
+$cards = fRecordSet::build('Card', array('uid=' => $_GET['cardid']));
+if($cards->count() == 0) {
+    //TODO: add card
+    exit;
+}
+$card = $cards[0];
+
 ?>
+<h2>Membership Management</h2>
 Card ID: <?=$_GET['cardid']?>
+
+
+<a href="/kiosk/" class="btn btn-default">Go back</a>
 <?require('./footer.php')?>
--- london.hackspace.org.uk/kiosk/index.php
+++ london.hackspace.org.uk/kiosk/index.php
@@ -4,7 +4,7 @@ require('./header.php');
 <h2>London Hackspace Membership Kiosk</h2>
 <p>This kiosk will soon allow you to add your RFID card, print Do Not Hack labels, and do other useful things. We're still working on it - if you're interested talk to Russ or Jasper.</p>
 
-<form action="card.php">
+<form action="card.php" style="position:absolute; left:-9999px;">
 <input type="text" name="cardid" id="cardid" accesskey="i"/>
 </form>
 <? require('./footer.php'); ?>
