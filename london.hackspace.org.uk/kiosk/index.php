aead2ce2d2bcba3f915be50171e4266c7a3084ef
Russ Garrett <russ@garrett.co.uk>
2014-11-09 00:23:47 +0000

    Membership kiosk placeholder

---
+++ london.hackspace.org.uk/kiosk/index.php
@@ -0,0 +1,9 @@
+<?
+$page = 'kiosk';
+require('header.php');
+?>
+<h2>London Hackspace Membership Kiosk</h2>
+<p>This kiosk will soon allow you to add your RFID card, print Do Not Hack labels, and do other useful things. We're still working on it - if you're interested talk to Russ or Jasper.</p>
+<? require('footer.php'); ?>
+</body>
+</html>
