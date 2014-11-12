d5d117073501b22697dc1e0bd296c9dd1b2e1510
Russ Garrett <russ@garrett.co.uk>
2014-11-12 00:01:10 +0000

    Fix case of card ID

--- london.hackspace.org.uk/kiosk/card.php
+++ london.hackspace.org.uk/kiosk/card.php
@@ -1,9 +1,10 @@
 <?
 $title = 'Membership Management';
 require('./header.php');
-$cards = fRecordSet::build('Card', array('uid=' => $_GET['cardid']));
+$cardid = strtoupper($_GET['cardid']);
+$cards = fRecordSet::build('Card', array('uid=' => $cardid));
 if($cards->count() == 0) {
-    fURL::redirect("/kiosk/addcard.php?cardid=" . $_GET['cardid']);
+    fURL::redirect("/kiosk/addcard.php?cardid=" . $cardid);
 }
 $card = $cards->getRecord(0);
 $user = new User($card->getUserId());
@@ -19,11 +20,11 @@ Subscribed: <?=$user->isMember() ? "Yes":"No"?>
 
 <h2>Stickers!:</h2>
 
-<p><a href="/kiosk/storage.php?cardid=<?=$_GET['cardid']?>" class="btn btn-success">Storage Requests</a></p>
-<p><a href="/kiosk/box.php?cardid=<?=$_GET['cardid']?>" class="btn btn-success">Member Box</a></p>
-<p><a href="/kiosk/fixme.php?cardid=<?=$_GET['cardid']?>" class="btn btn-success">Fix Me</a></p>
-<p><a href="/kiosk/hackme.php?cardid=<?=$_GET['cardid']?>" class="btn btn-success">Hack Me</a></p>
-<p><a href="/kiosk/nod.php?cardid=<?=$_GET['cardid']?>" class="btn btn-success">Notice of Disposal</a></p>
+<p><a href="/kiosk/storage.php?cardid=<?=$cardid?>" class="btn btn-success">Storage Requests</a></p>
+<p><a href="/kiosk/box.php?cardid=<?=$cardid?>" class="btn btn-success">Member Box</a></p>
+<p><a href="/kiosk/fixme.php?cardid=<?=$cardid?>" class="btn btn-success">Fix Me</a></p>
+<p><a href="/kiosk/hackme.php?cardid=<?=$cardid?>" class="btn btn-success">Hack Me</a></p>
+<p><a href="/kiosk/nod.php?cardid=<?=$cardid?>" class="btn btn-success">Notice of Disposal</a></p>
 <p><a href="/kiosk/" class="btn btn-default">Go back</a></p>
 
 
