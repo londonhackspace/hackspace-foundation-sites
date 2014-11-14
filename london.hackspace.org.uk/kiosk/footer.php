5dc35104f497781d9aa9d3898522a25b49bcd6fe
Russ Garrett <russ@garrett.co.uk>
2014-11-14 15:55:56 +0000

    60 second idle timeout on kiosk

--- london.hackspace.org.uk/kiosk/footer.php
+++ london.hackspace.org.uk/kiosk/footer.php
@@ -13,7 +13,21 @@
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/javascript/jquery.idle.min.js"></script>
 <script type="text/javascript" src="/javascript/kiosk.js"></script>
+
+<? if (isset($_GET['cardid'])) { ?>
+<script type="text/javascript">
+    $(document).idle({
+        onIdle: function() {
+            console.log("Idle");
+            window.location = "/kiosk";
+        },
+        timeout: 60000
+    });
+</script>
+<? } ?>
+
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
