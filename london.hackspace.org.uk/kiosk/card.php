716f19b0a783ec35496ab1a230e697870cd0f90b
Jasper Wallace <jasper@pointless.net>
2015-05-07 02:26:51 +0100

    add kiosk subscription date, fixes #88

--- london.hackspace.org.uk/kiosk/card.php
+++ london.hackspace.org.uk/kiosk/card.php
@@ -11,12 +11,32 @@ $card = $cards->getRecord(0);
 $user = new User($card->getUserId());
 $user->load();
 
+if ($user->isMember()) {
+    $result = fRecordSet::build(
+        'Transaction',
+        array('user_id=' => $user->getId(),
+            'timestamp>' => new fDate('2009-01-01'),
+            'timestamp<' => new fDate('now')
+        ),
+        array('timestamp' => 'asc')
+        );
+
+        $expires = strtotime($result[0]->getTimestamp());
+        # 30 days ~= a month
+        # we don't include the 14 days grace period here.
+        $expires += 30 * 24 * 60 * 60;
+        $expires = date('d F Y', $expires);
+}
+
 ?>
 <table class="table">
 <tr><th>Member Name</th><td><?=$user->prepareFullName()?></td></tr>
 <tr><th>Member ID</th><td> <?=$user->getMemberNumber()?></td></tr>
 <tr><th>Card ID</th><td> <?=$card->prepareUid()?></td></tr>
 <tr><th>Subscribed</th><td> <?=$user->isMember() ? "Yes":"No"?></td></tr>
+<? if ($user->isMember()) { ?>
+<tr><th>Subscription Expirey</th><td> <?=$expires ?></td></tr>
+<? } ?>
 </table>
 
 <h2>Print Membership Stickers</h2>
