4726c1e0de098e4237b6bb2794162b10495c21fb
Mark Steward <marksteward@gmail.com>
2015-10-10 15:47:29 +0000

    Use main hackspace forum for scraping by default
    Raised #113 to make this work in development

--- bin/storage-requests-phantomjs-ml-scrape.js
+++ bin/storage-requests-phantomjs-ml-scrape.js
@@ -2,7 +2,7 @@ var system = require('system');
 var args = system.args;
 var page = require('webpage').create();
 
-page.open('https://groups.google.com/forum/#!topicsearchin/london-hack-space-test/subject$3A%22Storage$20Request$20$23'+args[1]+'$3A%22', function() {
+page.open('https://groups.google.com/forum/#!topicsearchin/london-hack-space/subject$3A%22Storage$20Request$20$23'+args[1]+'$3A%22', function() {
 	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js", function() {
 		search = page.evaluate(function() { 
 			return $("table[role='list']").html().match(/\d+ post/g);
