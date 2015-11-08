b93fdb9f1a4daaa9d460cd7b776b392d341d0cff
Russ Garrett <russ@garrett.co.uk>
2015-11-08 23:09:29 +0000

    Fix another random script

--- london.hackspace.org.uk/members/subscription_stats.php
+++ london.hackspace.org.uk/members/subscription_stats.php
@@ -1,5 +1,6 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php');
-$average = $db->translatedQuery( 'SELECT AVG(amount) AS num FROM transactions WHERE timestamp >= date("now", "-30 day")' )->fetchRow();
+$average = $db->translatedQuery( "SELECT AVG(amount) AS num FROM transactions
+                                  WHERE timestamp >= now() - INTERVAL '1 month'" )->fetchRow();
 
 print "averageSubscription:{$average['num']}";
