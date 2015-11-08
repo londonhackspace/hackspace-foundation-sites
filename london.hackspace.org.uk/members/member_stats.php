246798fded8deaf06abc424d0abc620e795023cc
Russ Garrett <russ@garrett.co.uk>
2015-11-08 00:34:16 +0000

    Fix SQL for postgres

--- london.hackspace.org.uk/members/member_stats.php
+++ london.hackspace.org.uk/members/member_stats.php
@@ -1,6 +1,6 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php');
-$subscribers = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=1' )->fetchRow();
-$pending = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=0' )->fetchRow();
+$subscribers = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=true' )->fetchRow();
+$pending = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=false' )->fetchRow();
 
 print "subscribed:{$subscribers['num']} pending:{$pending['num']}";
