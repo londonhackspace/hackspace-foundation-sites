2b41e5697bceceeb72eb2eed78d6e2a9b1533aad
Sarah Simmonds <sarah@chixor.net>
2016-01-21 16:50:43 +1100

    Fixed retention stats to work with postgres database. Also updated to use monthly data rather than quarterly to better keep track of recent events.

--- london.hackspace.org.uk/stats/retention.php
+++ london.hackspace.org.uk/stats/retention.php
@@ -12,10 +12,13 @@ if (!$user->isMember()) {
 <div class="stats-page">
 <h2>Member Retention</h2>
 <?
-$length = 3;
+$length = 1;
 
-// get last quarter
-$month = floor(date('n')/3)*3;
+// get the last period
+$month = floor(date('n')/$length)*$length;
+if($length == 1)
+	$month--;
+
 $year = date('Y');
 if($month == 0) {
 	$month = 12;
@@ -33,35 +36,54 @@ if($pastMonth == 0) {
 $graph = Array();
 
 // iterate for enough quarters to show back to q1 2011
-$gofor = (((int)date('Y')-2011)*4) + floor(date('n')/3);
+$gofor = (((int)date('Y')-2011)*12) + floor(date('n')/$length);
 for($x=0;$x<$gofor;$x++) {
-	$period_start = $year.'-'.str_pad($month-($length-1), 2, '0', STR_PAD_LEFT);
+	$period_start =      $year.'-'.str_pad($month-($length-1), 2, '0', STR_PAD_LEFT);
+	$period_start_date = $year.'-'.str_pad($month-($length-1), 2, '0', STR_PAD_LEFT).'-01';
+	$end_year = $year;
+	$end_month = $month + 1;
+	if ($end_month == 13) {
+		$end_year++;
+		$end_month = 1;
+	}
 	$period_end = $year.'-'.str_pad($month, 2, '0', STR_PAD_LEFT);
+	$period_end_date = $end_year.'-'.str_pad($end_month, 2, '0', STR_PAD_LEFT).'-01';
+
 	$prev_start = $pastYear.'-'.str_pad($pastMonth-($length-1), 2, '0', STR_PAD_LEFT);
+	$prev_start_date = $pastYear.'-'.str_pad($pastMonth-($length-1), 2, '0', STR_PAD_LEFT).'-01';
+	$past_end_year = $pastYear;
+	$past_end_month = $pastMonth + 1;
+	if ($past_end_month == 13) {
+		$past_end_year++;
+		$past_end_month = 1;
+	}
 	$prev_end = $pastYear.'-'.str_pad($pastMonth, 2, '0', STR_PAD_LEFT);
+	$prev_end_date = $past_end_year.'-'.str_pad($past_end_month, 2, '0', STR_PAD_LEFT).'-01';
 
 	// get the number of members for last period
-	$csQuery = $db->translatedQuery( "SELECT COUNT(DISTINCT user_id) FROM transactions WHERE timestamp >= '$prev_start' AND timestamp <= '$prev_end'" );
+	$csQuery = $db->translatedQuery( "SELECT COUNT(DISTINCT user_id) FROM transactions WHERE timestamp >= '$prev_start_date' AND timestamp < '$prev_end_date'" );
 	$cs = (int)$csQuery->fetchScalar();
 
 	// get the number of members for this period
-	$ceQuery = $db->translatedQuery( "SELECT COUNT(DISTINCT user_id) FROM transactions WHERE timestamp >= '$period_start' AND timestamp <= '$period_end'" );
+	$ceQuery = $db->translatedQuery( "SELECT COUNT(DISTINCT user_id) FROM transactions WHERE timestamp >= '$period_start_date' AND timestamp < '$period_end_date'" );
 	$ce = (int)$ceQuery->fetchScalar();
 
 	// get the number of members who's first payment was this period
-        $cnQuery = $db->translatedQuery("SELECT COUNT(*) FROM (SELECT user_id, MIN(timestamp) first FROM transactions GROUP BY user_id)
-                        WHERE first >= '$period_start' and first <= '$period_end'");
+        $cnQuery = $db->translatedQuery("SELECT COUNT(*) FROM (SELECT user_id, MIN(timestamp) AS first FROM transactions GROUP BY user_id) x
+                        WHERE first >= '$period_start_date' and first < '$period_end_date'");
 	$cn = (int)$cnQuery->fetchScalar();
 
 	// get the number of members who's first payment was before last period, they didn't pay last period but are paying this period
-	$crQuery = $db->translatedQuery( "SELECT user_id, (SELECT min(timestamp) FROM transactions AS t2 WHERE t1.user_id = t2.user_id) AS start 
-			FROM transactions AS t1 
-			WHERE timestamp >= '$period_start' AND timestamp <= '$period_end' AND start < '$period_start' AND user_id NOT IN (SELECT user_id FROM transactions AS t4 WHERE timestamp >= '$prev_start' AND timestamp <= '$prev_end') 
-			GROUP BY user_id ORDER BY user_id" );
+	$crQuery = $db->translatedQuery( "SELECT user_id
+				FROM (SELECT user_id, MIN(timestamp) AS start FROM transactions GROUP BY user_id) as t2 natural join transactions as t1
+				WHERE timestamp >= '$period_start_date' AND timestamp < '$period_end_date' AND start < '$period_start_date' AND user_id NOT IN (SELECT user_id FROM transactions AS t4 WHERE timestamp >= '$prev_start_date' AND timestamp < '$prev_end_date') 
+				GROUP BY user_id ORDER BY user_id" );
+
+
 	$cr = $crQuery->countReturnedRows();
 
 	// get the number of members who paid last period but haven't paid this period
-	$cdQuery = $db->translatedQuery( "SELECT user_id FROM transactions WHERE timestamp >= '$prev_start' AND timestamp <= '$prev_end' AND user_id NOT IN (SELECT user_id FROM transactions WHERE timestamp >= '$period_start' AND timestamp <= '$period_end') GROUP BY user_id ORDER BY user_id" );
+	$cdQuery = $db->translatedQuery( "SELECT user_id FROM transactions WHERE timestamp >= '$prev_start_date' AND timestamp < '$prev_end_date' AND user_id NOT IN (SELECT user_id FROM transactions WHERE timestamp >= '$period_start_date' AND timestamp < '$period_end_date') GROUP BY user_id ORDER BY user_id" );
 	$cd = $cdQuery->countReturnedRows();
 
 	$graph[$period_start] = Array(
@@ -79,7 +101,8 @@ for($x=0;$x<$gofor;$x++) {
 
 	// cross checking
 	if((($cn + $cr) - $cd) != ($ce - $cs)) {
-		echo "<div class=\"alert alert-warning\">Numbers don't add up for period $period</div>";
+		echo "<div class=\"alert alert-warning\">Numbers don't add up for period $period_start_date - $period_end_date<br/>
+		(cn($cn) + cr($cr)) - cd($cd) != ce($ce) - cs($cs)</div>";
 	}
 
 	$month = $month-$length;
