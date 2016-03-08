add2eb075e4f276e34b0c4c9b5bd8837b12f5b1a
Jasper Wallace <jasper@pointless.net>
2016-03-08 18:58:05 +0000

    make the badge stuff work

--- london.hackspace.org.uk/kiosk/badge.php
+++ london.hackspace.org.uk/kiosk/badge.php
@@ -46,16 +46,15 @@ if (isset($_POST['print']) && $user->isMember()) {
     }
 
     $data_string = json_encode($data);
-    echo $data_string;
-#    $ch = curl_init('http://kiosk.london.hackspace.org.uk:12345/print/badge');
-#    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-#    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-#    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-#    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-#                'Content-Type: application/json',
-#                'Content-Length: ' . strlen($data_string)));
-#    $result = curl_exec($ch);
-#    curl_close($ch);
+    $ch = curl_init('http://kiosk.london.hackspace.org.uk:12345/print/badge');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string)));
+    $result = curl_exec($ch);
+    curl_close($ch);
     echo("<p>Your sticker is being printed now.</p>");
 }
 
