5d2b2dc72b8e67d44574086779edf24554ce4d00
Mark Steward <marksteward@gmail.com>
2017-08-19 23:09:02 +0000

    Remove unused code

--- main/templatetags/menulink.py
+++ main/templatetags/menulink.py
@@ -13,32 +13,3 @@ def menulink(context, name, title, viewname, *args, **kwargs):
         return format_html('<li><a href="{}">{}</a></li>', path, title)
 
 
-class PageNameNode(template.Node):
-    """
-    We need to rework the menu system, but for now this allows us to
-    reproduce the same per-template page name functionality from PHP
-    """
-    def __init__(self, page):
-        self.page = page
-
-    def render(self, context):
-        context['page'] = self.page
-        return ''
-
-def do_page_name(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, format_string = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError(
-            "%r tag requires a single argument" % token.contents.split()[0]
-        )
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError(
-            "%r tag's argument should be in quotes" % tag_name
-        )
-    return PageNameNode(format_string[1:-1])
-
-register.tag('page', do_page_name)
-
-
