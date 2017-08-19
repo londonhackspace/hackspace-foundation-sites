e0118adf8bb6edfe1f4644bfc33239ecea63478f
Mark Steward <marksteward@gmail.com>
2017-08-19 22:52:00 +0000

    Add STATIC_ROOT config

--- lhs/settings.py
+++ lhs/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 FLOURISH_LOOPBACK_URLS = {
     'authenticate': 'https://%s/session.php' % DOMAIN_NAME,
