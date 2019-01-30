89c8a662ee4674ac22f97f4e405cbccd9f06537c
tgreer <t6uav@icloud.com>
2019-01-30 11:31:35 +0000

    Update camera.php
    fix url

--- london.hackspace.org.uk/members/camera.php
+++ london.hackspace.org.uk/members/camera.php
@@ -1,7 +1,7 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php');
 
-$host = 'https://zoneminder.lan.london.hackspace.org.uk';
+$host = 'https://zoneminder.lan.london.hackspace.org.uk/zm';
 
 if (!$user || !$user->isMember()) {
     header('HTTP/1.1 403 Forbidden');
