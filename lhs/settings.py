d423e515dd4949231666eccc092aa5839ba144dc
Michael Cullen <michael@cullen-online.com>
2020-01-06 14:48:54 +0000

    Added payment dates to gocardless

--- lhs/settings.py
+++ lhs/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
+    'django_humanize',
     'absoluteuri',
     'lhsauth',
     'main',
