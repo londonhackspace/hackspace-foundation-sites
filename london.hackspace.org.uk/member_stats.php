49f16ae7a495f1ed924bdeb92ff91946037436a3
Michael Cullen <michael@cullen-online.com>
2020-02-22 17:06:59 +0000

    Fixed member stats last reconcile date
    another place referencing the transactions table

--- london.hackspace.org.uk/member_stats.php
+++ london.hackspace.org.uk/member_stats.php
@@ -4,7 +4,7 @@
 
     $subscribers = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=true' )->fetchRow();
     $pending = $db->translatedQuery( 'SELECT COUNT(id) AS num FROM users WHERE subscribed=false' )->fetchRow();
-    $last = $db->query('select EXTRACT(epoch FROM max(timestamp)) AS num from transactions')->fetchRow();
+    $last = $db->query('select EXTRACT(epoch FROM max(timestamp)) AS num from lhspayments_payment')->fetchRow();
  
 if($_GET['format'] == "json") { 
     ?>{"subscribed": <?php print($subscribers['num']); ?>}<?php
