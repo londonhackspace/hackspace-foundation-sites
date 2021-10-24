2f3393b1d036085c6f7d5faabd96c8fb75598df8
Simon Hewison <simon.hewison@exactly-that.co.uk>
2021-10-24 15:58:27 +0100

    camera ancient PHP workaround for now, ignore SSL verification

--- london.hackspace.org.uk/members/camera.php
+++ london.hackspace.org.uk/members/camera.php
@@ -1,5 +1,5 @@
 <?php
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../lib/init.php';
 
 $host = 'https://zoneminder.lan.london.hackspace.org.uk/zm';
 
@@ -18,13 +18,19 @@ if (!isset($_GET['scale'])) {
     $scale = intval($_GET['scale']);
 }
 
-
 header('Content-Type: image/jpeg');
 
 $chan = intval($_GET['id']);
 
 $url = "$host/cgi-bin/nph-zms?mode=single&scale=$scale&monitor=$chan";
 
-echo(file_get_contents("$host/cgi-bin/nph-zms?mode=single&scale=$scale&monitor=$chan"));
+// Workaround for ancient PHP not being able to fetch from zoneminder
 
-?>
+$arrContextOptions = array(
+    "ssl" => array(
+        "verify_peer" => false,
+        "verify_peer_name" => false
+    )
+);
+
+echo (file_get_contents("$host/cgi-bin/nph-zms?mode=single&scale=$scale&monitor=$chan", false, stream_context_create($arrContextOptions)));
