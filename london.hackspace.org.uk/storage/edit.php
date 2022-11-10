361c69bf4352689753bd051ddbe7a795b8d3d076
padski <padski@padski.co.uk>
2022-11-10 22:37:32 +0000

    disable properly?

--- london.hackspace.org.uk/storage/edit.php
+++ london.hackspace.org.uk/storage/edit.php
@@ -1,6 +1,6 @@
 <?
 $page = 'storagedetails_edit';
-$title = "Storage request";
+$title = "Storage request";0
 require( '../header.php' );
 
 ensureMember();
@@ -186,7 +186,7 @@ if (isset($_POST['token'])) {
             </div>
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-9">
-                    <input type="submit" id="formSubmit" name="submitForm" value="Submit request" class="btn btn-primary"/>
+                        <//input type="submit" id="formSubmit" name="submitForm" value="Submit request" class="btn btn-primary"/>
                 </div>
             </div>
         </form>
