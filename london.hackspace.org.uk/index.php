0df9ac3095704b6c2aa63dafb31e2733815a05a4
Padski <1208766+padski@users.noreply.github.com>
2024-10-11 15:05:08 +0100

    Not 'interim' address

--- london.hackspace.org.uk/index.php
+++ london.hackspace.org.uk/index.php
@@ -60,7 +60,7 @@ require 'header.php';?>
             <h3>Location</h3>
             <div>
               <p>
-                Our "interim" address is:
+                Our address is:
               </p>
               <p>
                 41–43 Standard Road<br>
